55c0b7b9a2d8e5be154142eb7cf6fce5cef75819
Andrei Alves Fracalossi <124471216+andreihmmm@users.noreply.github.com>
2023-06-19 19:23:56 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -15,15 +15,69 @@ window.onload = function () {
     }))
 }
 
-function buscarPorTalentos() {
-    buscarPorTalentos = document.querySelector("#buscar-talentos")
+function grupos() {
+    var grupos = document.querySelector('#grupos.a-esquerda')
+    var elementosAtivos = document.querySelectorAll('.a-esquerda.active');
+    var principaisAtivos = document.querySelectorAll('.principais.active');
+    var gruposdiv = document.querySelector('#gruposdiv')
 
-    principais = document.querySelectorAll(".principais")
-    while (principais.classList.length > 1) {
-        if (principais.classList.item(0) !== '.principais')
-            principais.classList.remove(principais.classList.item(0));
+    for (var i = 0; i < principaisAtivos.length; i++) {
+        principaisAtivos[i].classList.remove('active');
+    }
+    for (var i = 0; i < elementosAtivos.length; i++) {
+        elementosAtivos[i].classList.remove('active');
+    }
+
+    gruposdiv.classList.add('active')
+    grupos.classList.add('active')
+}
+
+function eventos() {
+    var eventos = document.querySelector('#eventos.a-esquerda')
+    var elementosAtivos = document.querySelectorAll('.a-esquerda.active');
+    var principaisAtivos = document.querySelectorAll('.principais.active')
+    var eventosdiv = document.querySelector('#eventosdiv')
+
+    for (var i = 0; i < principaisAtivos.length; i++) {
+        principaisAtivos[i].classList.remove('active');
+    }
+
+    for (var i = 0; i < elementosAtivos.length; i++) {
+        elementosAtivos[i].classList.remove('active');
+    }
+
+    eventosdiv.classList.add('active')
+    eventos.classList.add('active')
+}
+function notificacoes() {
+    var notificacoes = document.querySelector('#notificacoes.a-esquerda')
+    var elementosAtivos = document.querySelectorAll('.a-esquerda.active');
+    var principaisAtivos = document.querySelectorAll('.principais.active')
+
+    for (var i = 0; i < principaisAtivos.length; i++) {
+        principaisAtivos[i].classList.remove('active');
+    }
+
+    for (var i = 0; i < elementosAtivos.length; i++) {
+        elementosAtivos[i].classList.remove('active');
+    }
+
+    notificacoes.classList.add('active')
+}
+function buscarPorTalentos() {
+    var buscarPorTalentos = document.querySelector('#buscarPorTalentos.a-esquerda')
+    var elementosAtivos = document.querySelectorAll('.a-esquerda.active');
+    var buscarTalentos = document.querySelector("#buscarTalentos")
+    var principaisAtivos = document.querySelectorAll('.principais.active')
+
+    for (var i = 0; i < principaisAtivos.length; i++) {
+        principaisAtivos[i].classList.remove('active');
+    }
+
+    for (var i = 0; i < elementosAtivos.length; i++) {
+        elementosAtivos[i].classList.remove('active');
     }
 
     buscarPorTalentos.classList.add('active')
-
+    buscarTalentos.classList.add('active')
 }
